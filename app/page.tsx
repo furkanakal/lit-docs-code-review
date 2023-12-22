94aa06ac1290ba645db9c4e43b1defe24f46b474
Furkan <furkanakaldev@gmail.com>
2023-12-22 15:04:29 +0300

    add: signAndSaveAuthMessage()

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,12 @@ import { useState } from 'react';
 import { LitNodeClient } from '@lit-protocol/lit-node-client';
 import * as LitJsSdk from '@lit-protocol/lit-node-client';
 
+import { ethers } from 'ethers';
+import { ethConnect } from '@lit-protocol/auth-browser';
+import { LitActionResource } from '@lit-protocol/auth-helpers';
+
+declare var window: any
+
 export default function Home() {
   const [status, setStatus] = useState('');
 
@@ -18,14 +24,41 @@ export default function Home() {
 
     await litNodeClient.connect();
 
-    setStatus('Getting authSig...');
+    // -----------------------------------------------------------
+
+    // // checkAndSignAuthMessage
+
+    // setStatus('Getting authSig...');
+
+    // const nonce_ = litNodeClient.getLatestBlockhash()!;
+
+    // const authSig = await LitJsSdk.checkAndSignAuthMessage({
+    //   chain: "ethereum",
+    //   nonce: nonce_
+    // })
+
+    // console.log(authSig);
+
+    // -----------------------------------------------------------
+
+    // // signAndSaveAuthMessage
+
+    const web3Provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = web3Provider.getSigner()
+    const walletAddress = await signer.getAddress()
 
     const nonce_ = litNodeClient.getLatestBlockhash()!;
+    const uri_ = "http://localhost:3000/";
 
-    const authSig = await LitJsSdk.checkAndSignAuthMessage({
-      chain: "ethereum",
+    const authSig = await ethConnect.signAndSaveAuthMessage({
+      web3: web3Provider,
+      account: walletAddress,
+      chainId: 1,
+      resources: new LitActionResource('*'),
+      expiration: new Date(Date.now() + 1000 * 60 * 60 * 24).toISOString(),
+      uri: uri_,
       nonce: nonce_
-    })
+    });
 
     console.log(authSig);
 
